23cc49f0d0b4fdcfd76b073156ee1ac76d27ab70
vaibhav21898 <vj2598054@gmail.com>
2024-07-19 12:52:46 +0530

    pdf list ui complete

--- src/pages/pdf-lists/index.tsx
+++ src/pages/pdf-lists/index.tsx
@@ -2,7 +2,8 @@
 import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import MyDocument from "@/components/MyDocument";
-
+import PublicLayout from "@/components/layout/Public";
+import { RiDeleteBin6Line } from "react-icons/ri";
 const PDFViewer = dynamic(
   () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
   { ssr: false }
@@ -18,59 +19,91 @@ interface PdfData {
 
 const PdfList = () => {
   const [pdfs, setPdfs] = useState<PdfData[]>([]);
-  const [selectedPdf, setSelectedPdf] = useState<PdfData | null>(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("highToLow");
+  // const [selectedPdf, setSelectedPdf] = useState<PdfData | null>(null);
 
   useEffect(() => {
     const storedPdfs = JSON.parse(localStorage.getItem("pdfs") || "[]");
     setPdfs(storedPdfs);
   }, []);
+  const filteredPdfs = pdfs
+    .filter((pdf) =>
+      pdf.title.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    .sort((a, b) => {
+      if (sortOrder === "highToLow") {
+        return (
+          new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+        );
+      } else {
+        return (
+          new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime()
+        );
+      }
+    });
+  console.log(filteredPdfs);
 
-  const handleViewPdf = (pdf: PdfData) => {
-    setSelectedPdf(pdf);
+  const handleDelete = (index: number) => {
+    const updatedPdfs = [...pdfs];
+    updatedPdfs.splice(index, 1);
+    setPdfs(updatedPdfs);
+    localStorage.setItem("pdfs", JSON.stringify(updatedPdfs));
   };
 
   return (
-    <div className="pdf-list">
-      <h2 className="text-xl font-bold">Stored PDFs</h2>
-      {selectedPdf ? (
-        <div className="mt-4">
-          <h3 className="text-lg font-bold">{selectedPdf.title}</h3>
-          <PDFViewer width="600" height="400">
-            <MyDocument
-              text={selectedPdf.text}
-              title={selectedPdf.title}
-              imageUrl={selectedPdf.imageUrl}
-            />
-          </PDFViewer>
-          <button
-            onClick={() => setSelectedPdf(null)}
-            className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
-          >
-            Close PDF
-          </button>
+    <PublicLayout>
+      <section className="public-container flex flex-col gap-8">
+        <div className="flex gap-5">
+          <input
+            type="text"
+            placeholder="Search Pdf ...."
+            className="bg-transparent border outline-none py-2 text-sm text-white pl-4 rounded-full w-[400px]"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <div>
+            <select
+              name=""
+              id=""
+              value={sortOrder}
+              onChange={(e) => setSortOrder(e.target.value)}
+              className="bg-transparent text-white  border py-2 rounded-full  w-[200px] px-3 text-sm"
+            >
+              <option value="">High To Low</option>
+              <option value="">Low To High</option>
+            </select>
+          </div>
         </div>
-      ) : (
-        <ul>
-          {pdfs.map((pdf, index) => (
-            <li key={index} className="mb-4">
-              <div className="mb-2">
-                <strong>Title:</strong> {pdf.title}
+        <div className="flex gap-5">
+          <div className=" w-1/4 flex flex-col gap-y-3">
+            {filteredPdfs?.map((pdf, i) => (
+              <div key={i} className="bg-white p-4 rounded-xl">
+                <div className="flex justify-center">
+                  <img src="/pdf.png" alt="" className="h-24" />
+                </div>
+                <div className="flex flex-col gap-y-3">
+                  <p className="text-center text-xl">{pdf?.title}</p>
+                  <div className="flex w-full justify-between">
+                    <p className="text-xs">
+                      {new Date(pdf.timestamp).toLocaleString() ||
+                        "Date Not  Define"}
+                    </p>
+                    <span
+                      onClick={() => handleDelete(i)}
+                      className="text-red-700 cursor-pointer "
+                    >
+                      <RiDeleteBin6Line size={20} />
+                    </span>
+                  </div>
+                </div>
               </div>
-              <div className="mb-2">
-                <strong>Date:</strong>{" "}
-                {new Date(pdf.timestamp).toLocaleString()}
-              </div>
-              <button
-                onClick={() => handleViewPdf(pdf)}
-                className="mt-2 px-4 py-2 bg-green-500 text-white rounded"
-              >
-                View PDF
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+            ))}
+          </div>
+          <div className="border border-orange-500 w-[75%]"></div>
+        </div>
+      </section>
+    </PublicLayout>
   );
 };
 
